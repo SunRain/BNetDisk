9a26020b524e584255ba12c2fd242e255a5d67fb
SunRain <41245110@qq.com>
2016-06-19 14:04:21 +0800

    [BDiskCookieJar] save cookies to local storage

--- src/BDiskRequest/BDiskCookieJar.cpp
+++ src/BDiskRequest/BDiskCookieJar.cpp
@@ -1,6 +1,13 @@
 #include "BDiskCookieJar.h"
 
+#include <QCoreApplication>
 #include <QDebug>
+#include <QSettings>
+#include <QFile>
+#include <QTextStream>
+#include <QStandardPaths>
+#include <QDir>
+
 #include <QNetworkCookie>
 
 #define DBG_OUT 0
@@ -9,13 +16,51 @@ BDiskCookieJar::BDiskCookieJar(QObject *parent)
     : QNetworkCookieJar(parent)
     , m_locker(QMutex::Recursive)
 {
+    QString dataPath = QStandardPaths::writableLocation (QStandardPaths::DataLocation);
+    QDir dir(dataPath);
+    if (!dir.exists())
+        dir.mkpath(dataPath);
+    m_cookieFile = QString("%1/cookie").arg(dataPath);
+
+    QFile file(m_cookieFile);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))  {
+        qDebug()<<Q_FUNC_INFO<<"Can't open exist cookies files";
+    } else {
+        QByteArray line;
+        while (!file.atEnd()) {
+            line = file.readLine();
+            m_cookieList.append(QNetworkCookie::parseCookies(line));
+        }
+    }
+    file.close();
 
 }
 
-QHash<QByteArray, QByteArray> BDiskCookieJar::cookieList() const
+BDiskCookieJar::~BDiskCookieJar()
+{
+    QFile file(m_cookieFile);
+    if (file.exists()) {
+        if (!file.remove())
+            qDebug()<<Q_FUNC_INFO<<"Not remove file!!";
+    }
+
+    if (!file.open(QIODevice::WriteOnly)) {
+        qDebug()<<Q_FUNC_INFO<<"Can't open to write cookies";
+        return;
+    }
+    QTextStream out(&file);
+    foreach (QNetworkCookie c, m_cookieList) {
+        out<<c.toRawForm()<<"\n";
+    }
+    out.flush();
+    file.close();
+}
+
+QList<QNetworkCookie> BDiskCookieJar::cookieList() const
 {
     return m_cookieList;
 }
+
 
 QList<QNetworkCookie> BDiskCookieJar::cookiesForUrl(const QUrl &url) const
 {
@@ -31,45 +76,68 @@ QList<QNetworkCookie> BDiskCookieJar::cookiesForUrl(const QUrl &url) const
     return cookies;
 }
 
-bool BDiskCookieJar::setCookiesFromUrl(const QList<QNetworkCookie> &cookieList, const QUrl &url)
+//bool BDiskCookieJar::setCookiesFromUrl(const QList<QNetworkCookie> &cookieList, const QUrl &url)
+//{
+//    m_locker.lock();
+//#if DBG_OUT
+//    qDebug()<<Q_FUNC_INFO<<"cookiesForUrl "<<url;
+//#endif
+
+//    foreach (QNetworkCookie c, cookieList) {
+//#if DBG_OUT
+//        qDebug()<<Q_FUNC_INFO<<"cookies name="<<c.name ()<<" value="<<c.value ();
+//#endif
+//        if (!m_cookieList.contains(c)) {
+//            m_cookieList.append(c);
+//        }
+//    }
+//    m_locker.unlock();
+
+//    return QNetworkCookieJar::setCookiesFromUrl(cookieList, url);
+//}
+
+bool BDiskCookieJar::insertCookie(const QNetworkCookie &cookie)
 {
     m_locker.lock();
 #if DBG_OUT
-    qDebug()<<Q_FUNC_INFO<<"cookiesForUrl "<<url;
+    qDebug()<<Q_FUNC_INFO<<"cookies name="<<cookie.name ()<<" value="<<cookie.value ();
 #endif
-
-    foreach (QNetworkCookie c, cookieList) {
-#if DBG_OUT
-        qDebug()<<Q_FUNC_INFO<<"cookies name="<<c.name ()<<" value="<<c.value ();
-#endif
-        m_cookieList.insert(c.name(), c.value());
+    foreach (QNetworkCookie c, m_cookieList) {
+        if (c.hasSameIdentifier(cookie)) {
+            m_cookieList.removeOne(c);
+        }
     }
+    m_cookieList.append(cookie);
     m_locker.unlock();
-
-//    TokenProvider::instance ()->updateHackLoginCookies (cookieList);
-    return QNetworkCookieJar::setCookiesFromUrl(cookieList, url);
+    return QNetworkCookieJar::insertCookie(cookie);
 }
 
-//bool BDiskCookieJar::insertCookie(const QNetworkCookie &cookie)
-//{
-//#if DBG_OUT
-//    qDebug()<<Q_FUNC_INFO<<"cookies name="<<cookie.name ()<<" value="<<cookie.value ();
-//#endif
-//    return QNetworkCookieJar::insertCookie(cookie);
-//}
-
-//bool BDiskCookieJar::updateCookie(const QNetworkCookie &cookie)
-//{
-//#if DBG_OUT
-//    qDebug()<<Q_FUNC_INFO<<"cookies name="<<cookie.name ()<<" value="<<cookie.value ();
-//#endif
-//    return QNetworkCookieJar::updateCookie(cookie);
-//}
+bool BDiskCookieJar::updateCookie(const QNetworkCookie &cookie)
+{
+    m_locker.lock();
+#if DBG_OUT
+    qDebug()<<Q_FUNC_INFO<<"cookies name="<<cookie.name ()<<" value="<<cookie.value ();
+#endif
+    foreach (QNetworkCookie c, m_cookieList) {
+        if (c.hasSameIdentifier(cookie)) {
+            m_cookieList.removeOne(c);
+        }
+    }
+    m_cookieList.append(cookie);
+    m_locker.unlock();
+    return QNetworkCookieJar::updateCookie(cookie);
+}
 
 //bool BDiskCookieJar::deleteCookie(const QNetworkCookie &cookie)
 //{
+//    m_locker.lock();
 //#if DBG_OUT
 //    qDebug()<<Q_FUNC_INFO<<"cookies name="<<cookie.name ()<<" value="<<cookie.value ();
 //#endif
+//    foreach (QNetworkCookie c, m_cookieList) {
+//        if (c.hasSameIdentifier(cookie))
+//            m_cookieList.removeOne(c);
+//    }
+//    m_locker.unlock();
 //    return QNetworkCookieJar::deleteCookie(cookie);
 //}
--- src/BDiskRequest/BDiskCookieJar.h
+++ src/BDiskRequest/BDiskCookieJar.h
@@ -2,32 +2,33 @@
 #define BDISKCOOKIEJAR_H
 
 #include <QNetworkCookieJar>
-//#include <QNetworkCookie>
 
 #include <QMutex>
 #include <QList>
 
 #include "SingletonPointer.h"
 
+class QSettings;
 class BDiskCookieJar : public QNetworkCookieJar
 {
     Q_OBJECT
     DECLARE_SINGLETON_POINTER(BDiskCookieJar)
 
 public:
-    QHash<QByteArray, QByteArray> cookieList() const;
+    virtual ~BDiskCookieJar();
+    QList<QNetworkCookie> cookieList() const;
 
     // QNetworkCookieJar interface
     QList<QNetworkCookie> cookiesForUrl(const QUrl &url) const;
-    bool setCookiesFromUrl(const QList<QNetworkCookie> &cookieList, const QUrl &url);
+//    bool setCookiesFromUrl(const QList<QNetworkCookie> &cookieList, const QUrl &url);
+    bool insertCookie(const QNetworkCookie &cookie);
+    bool updateCookie(const QNetworkCookie &cookie);
+//    bool deleteCookie(const QNetworkCookie &cookie);
 
 private:
     QMutex m_locker;
-//    QList<QNetworkCookie> m_cookieList;
-    QHash<QByteArray, QByteArray> m_cookieList;
-//    bool insertCookie(const QNetworkCookie &cookie);
-//    bool updateCookie(const QNetworkCookie &cookie);
-//    bool deleteCookie(const QNetworkCookie &cookie);
+    QList<QNetworkCookie> m_cookieList;
+    QString m_cookieFile;
 };
 
 #endif // BDISKCOOKIEJAR_H
